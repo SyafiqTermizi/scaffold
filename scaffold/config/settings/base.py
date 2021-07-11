34ecbb22341ad6955de1ac73a8b50c9a20cb470c
syafiq <ahmadsyafiq93@gmail.com>
2021-07-11 13:25:37 +0800

    Chore: Configure mailhog

--- scaffold/config/settings/base.py
+++ scaffold/config/settings/base.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = "scaffold.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR.parent / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
