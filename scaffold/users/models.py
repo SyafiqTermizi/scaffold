62540b73b5a713e74b33c61ce27ebf343ca74f25
syafiq <ahmadsyafiq93@gmail.com>
2021-07-11 12:10:52 +0800

    Feature: Add email verification model

--- scaffold/users/models.py
+++ scaffold/users/models.py
@@ -1,6 +1,12 @@
+from functools import partial
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.crypto import get_random_string
 from django.utils.translation import gettext_lazy as _
+
+
+generate_token = partial(get_random_string, 250)
 
 
 class User(AbstractUser):
@@ -13,3 +19,11 @@ class User(AbstractUser):
 
     def __str__(self) -> str:
         return self.username
+
+
+class EmailVerificationToken(models.Model):
+    token = models.CharField(max_length=255, default=generate_token)
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.user.username
