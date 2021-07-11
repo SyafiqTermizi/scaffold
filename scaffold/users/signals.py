bfd1ff689308a9b1de0f8c57a24045931fa10b01
syafiq <ahmadsyafiq93@gmail.com>
2021-07-11 13:39:34 +0800

    Feature: Configure signal and celery task to send email

---
+++ scaffold/users/signals.py
@@ -0,0 +1,8 @@
+from django.core.mail import send_mail
+from .models import User
+from .tasks import create_token_and_send_email as bg_task
+
+
+def create_token_and_send_email(sender, instance: User, created: bool, **kwargs):
+    if created:
+        bg_task.delay(instance.pk)
