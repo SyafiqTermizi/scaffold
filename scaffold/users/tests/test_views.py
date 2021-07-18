d3cea1b0d44d21908eadae9887944b7508da5db6
syafiq_termizi <ahmadsyafiq93@gmail.com>
2021-07-18 09:53:35 +0800

    Chore: Test api views failing case

--- scaffold/users/tests/test_views.py
+++ scaffold/users/tests/test_views.py
@@ -3,7 +3,7 @@ import pytest
 from scaffold.users.views import UserCreationAPIView
 
 
-def test_user_creation_api_view(db, api_client):
+def test_user_creation_api_view_success(db, api_client):
     """
     If a user creation is successful, the endpoint should:
     1. return status 201
@@ -25,3 +25,81 @@ def test_user_creation_api_view(db, api_client):
 
     with pytest.raises(KeyError):
         response.json()["password_1"]
+
+
+@pytest.mark.parametrize(
+    "username,email,password_1,password_2,error",
+    [
+        pytest.param(
+            "existinguser",
+            "newemail@test.com",
+            "matching_password",
+            "matching_password",
+            {"username": ["A user with that username already exists."]},
+        ),
+        pytest.param(
+            "newusername",
+            "existinguser@test.com",
+            "matching_password",
+            "matching_password",
+            {"email": ["user with this email address already exists."]},
+        ),
+        pytest.param(
+            "newusername",
+            "newemail@test.com",
+            "matching_password",
+            "unmatching_password",
+            {"non_field_errors": ["Password Not Match"]},
+        ),
+        pytest.param(
+            "",
+            "newemail@test.com",
+            "matching_password",
+            "matching_password",
+            {"username": ["This field may not be blank."]},
+        ),
+        pytest.param(
+            "newusername",
+            "",
+            "matching_password",
+            "matching_password",
+            {"email": ["This field may not be blank."]},
+        ),
+        pytest.param(
+            "invalidusername!",
+            "invalidemail",
+            "matching_password",
+            "matching_password",
+            {
+                "username": [
+                    "Enter a valid username. This value may contain only letters, numbers, and @/./+/-/_ characters."
+                ],
+                "email": ["Enter a valid email address."],
+            },
+        ),
+    ],
+)
+def test_user_creation_api_view_fail(
+    db,
+    api_client,
+    create_user,
+    username,
+    email,
+    password_1,
+    password_2,
+    error,
+):
+    create_user(username="existinguser", email="existinguser@test.com")
+
+    response = api_client().post(
+        "/users/register/",
+        data={
+            "username": username,
+            "email": email,
+            "password_1": password_1,
+            "password_2": password_2,
+        },
+        format="json",
+    )
+    assert response.status_code == 400
+    assert response.json() == error
